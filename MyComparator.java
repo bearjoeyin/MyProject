e872aec8f4bf0714c402c2a674f00a2005235434
Xue Yin <xue.j.yin@gmail.com>
2015-10-30 14:49:52 -0700

    modified 1

--- MyComparator.java
+++ MyComparator.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class MyComparator implements Comparator{
+public class MyComparator implements Comparator<Object>{
 	@Override
 	public int compare(Object o1, Object o2) {
 		int i=0;
--- Sort.java
+++ Sort.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 public class Sort {
 
+	private static MyComparator mc;
+
+	
 	public static void main(String[] args) {
-		Arrays.sort(args, new MyComparator());
+
+		mc = new MyComparator();
+		Arrays.sort(args, mc);
 		for (int i = 0; i < args.length; i++){
 			System.out.println(args[i]);
 		}
